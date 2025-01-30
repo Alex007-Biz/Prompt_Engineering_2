cce812174d3ac41e77dca85c1017667e68874a91
Aleksey <bizukin@yandex.ru>
2025-01-31 02:04:12 +0300

    1

--- PE_4.3_Bot_with_TTS/voice.py
+++ PE_4.3_Bot_with_TTS/voice.py
@@ -1,25 +1,14 @@
 from elevenlabs import play
 from elevenlabs.client import ElevenLabs
-from config import elevenlabs_api_key
-from pydub import AudioSegment
-from pydub.playback import play as pydub_play
+import config
 
 client = ElevenLabs(
- api_key=elevenlabs_api_key,
+ api_key=config.elevenlabs_api_key,
 )
-print(f"config.elevenlabs_api_key: {elevenlabs_api_key}")
+
 audio = client.generate(
   text="Привет! Меня зовут Дейв.",
   voice="Dave",
   model="eleven_multilingual_v2"
 )
-
-# play(audio)
-
-# Сохраняем аудио в файл
-with open("output.mp3", "wb") as f:
-    f.write(audio)
-
-# Воспроизводим аудио через pydub
-audio_segment = AudioSegment.from_file("output.mp3")
-pydub_play(audio_segment)
+play(audio)
