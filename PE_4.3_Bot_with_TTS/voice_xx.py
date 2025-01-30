8b62facfae91aa623acb602fb95dde45ed7b67ee
Aleksey <bizukin@yandex.ru>
2025-01-31 02:22:12 +0300

    1

--- PE_4.3_Bot_with_TTS/voice_xx.py
+++ PE_4.3_Bot_with_TTS/voice_xx.py
@@ -1,13 +1,11 @@
-from elevenlabs import play
 from elevenlabs.client import ElevenLabs
-from config import elevenlabs_api_key
-from pydub import AudioSegment
-from pydub.playback import play as pydub_play
+import config
+from playsound import playsound
 
+# Инициализация клиента ElevenLabs
 client = ElevenLabs(
-    api_key=elevenlabs_api_key,
+    api_key=config.elevenlabs_api_key,
 )
-print(f"config.elevenlabs_api_key: {elevenlabs_api_key}")
 
 # Генерация аудио
 audio = client.generate(
@@ -16,13 +14,9 @@ audio = client.generate(
     model="eleven_multilingual_v2"
 )
 
-# Преобразуем генератор в байты
-audio_bytes = b"".join(audio)  # Собираем все части генератора в один байтовый объект
-
 # Сохраняем аудио в файл
 with open("output.mp3", "wb") as f:
-    f.write(audio_bytes)
+    f.write(audio)
 
-# Воспроизводим аудио через pydub
-audio_segment = AudioSegment.from_file("output.mp3")
-pydub_play(audio_segment)
+# Воспроизводим аудио через playsound
+playsound("output.mp3")м
