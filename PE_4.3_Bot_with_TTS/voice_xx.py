d05af473c803158996fb987fe01ddffd10a2f7bf
Aleksey <bizukin@yandex.ru>
2025-01-31 02:37:28 +0300

    1

--- PE_4.3_Bot_with_TTS/voice_xx.py
+++ PE_4.3_Bot_with_TTS/voice_xx.py
@@ -1,6 +1,18 @@
 from elevenlabs.client import ElevenLabs
 import config
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.utils import which
+from io import BytesIO
+import sounddevice as sd
+import numpy as np
+
+# Указываем путь к ffmpeg и ffprobe вручную
+ffmpeg_path = "C:\\ffmpeg\\bin\\ffmpeg.exe"
+ffprobe_path = "C:\\ffmpeg\\bin\\ffprobe.exe"
+
+# Устанавливаем пути для pydub
+AudioSegment.converter = which(ffmpeg_path)
+AudioSegment.ffprobe = which(ffprobe_path)
 
 # Инициализация клиента ElevenLabs
 client = ElevenLabs(
@@ -14,9 +26,15 @@ audio = client.generate(
     model="eleven_multilingual_v2"
 )
 
-# Сохраняем аудио в файл
-with open("output.mp3", "wb") as f:
-    f.write(audio)
+# Собираем данные из генератора в один байтовый объект
+audio_bytes = b"".join(audio)  # Преобразуем генератор в байты
 
-# Воспроизводим аудио через playsound
-playsound("output.mp3")м
+# Преобразуем аудио в формат, понятный pydub
+audio_segment = AudioSegment.from_file(BytesIO(audio_bytes))
+
+# Преобразуем аудио в массив numpy для sounddevice
+samples = np.array(audio_segment.get_array_of_samples())
+
+# Воспроизводим аудио через sounddevice
+sd.play(samples, audio_segment.frame_rate)
+sd.wait()  # Ждем окончания воспроизведения
